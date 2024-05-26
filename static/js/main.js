4e1ca43b625a7becc711972d905778a2ab9224b1
Gonzalo Duarte <89230767+duartegonzaloariel@users.noreply.github.com>
2024-05-26 06:32:29 -0300

    segundo commit
    se sube carpeta static

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click',() =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+ScrollReveal().reveal('.nav-main',{delay:200});
+ScrollReveal().reveal('.nav-main2',{delay:400});
+ScrollReveal().reveal('.baner-one',{delay:600});
+ScrollReveal().reveal('.news-card',{delay:300});
+ScrollReveal().reveal('.social',{delay:400});
+ScrollReveal().reveal('.footer-links',{delay:500});
